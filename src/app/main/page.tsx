240f62500c0e943f27765a948907a8ead91fdd7d
jesibabaoclipdata-a11y <jesibabaoclipdata@gmail.com>
2026-05-30 00:27:39 +0800

    bg commit

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,11 +1,13 @@
 import HeroSectionPage from "@/app/hero-section-41/page";
 import ProductListingPage from "@/app/product-listing-01/page";
 import { FeatureBento } from "@/components/feature-bento";
+import { BackgroundRippleEffect } from "@/components/ui/background-ripple-effect";
 
 export default function MainPage() {
 
     return(
         <>
+
         <HeroSectionPage/>
          <FeatureBento/>
         <ProductListingPage/>
