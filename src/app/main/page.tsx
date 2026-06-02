15a43e02ce0a51cc508b0d7fd6d5c4cb0b6c6f9c
jesibabaoclipdata-a11y <jesibabaoclipdata@gmail.com>
2026-06-02 18:17:41 +0800

    small chamges

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,14 +1,8 @@
 import HeroSectionPage from "@/app/hero-section-41/page";
 import ProductListingPage from "@/app/product-listing-01/page";
 import { FeatureBento } from "@/components/feature-bento";
-<<<<<<< HEAD
-
 import { Feature72 } from "@/components/feature72";
-
-=======
 import { BackgroundRippleEffect } from "@/components/ui/background-ripple-effect";
->>>>>>> 240f62500c0e943f27765a948907a8ead91fdd7d
-
 export default function MainPage() {
 
     return(
