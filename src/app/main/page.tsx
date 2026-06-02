32c984c16d44307a8b70325a2e88b1ee591fc8c1
jesibabaoclipdata-a11y <jesibabaoclipdata@gmail.com>
2026-06-02 18:01:06 +0800

    06/02/26 commit

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,14 +1,20 @@
 import HeroSectionPage from "@/app/hero-section-41/page";
 import ProductListingPage from "@/app/product-listing-01/page";
 import { FeatureBento } from "@/components/feature-bento";
+import { Feature197 } from "@/components/feature197";
+import { Feature72 } from "@/components/feature72";
+import { features } from "process";
 
 export default function MainPage() {
 
     return(
         <>
         <HeroSectionPage/>
-         <FeatureBento/>
+         < Feature72 />
+       
+          
         <ProductListingPage/>
+      
 
 
 
